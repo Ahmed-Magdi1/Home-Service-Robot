b84fba833a2fddac6a8e1bdd039df7f667e08402
Ahmed-Magdi1 <Ahmed216123@gmail.com>
2024-07-28 03:03:15 +0300

    Removing unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmed/Home_Service_Robot_ws/src
-# Build directory: /home/ahmed/Home_Service_Robot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("teleop_twist_keyboard")
-subdirs("my_robot")
--- build/teleop_twist_keyboard/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmed/Home_Service_Robot_ws/src/teleop_twist_keyboard
-# Build directory: /home/ahmed/Home_Service_Robot_ws/build/teleop_twist_keyboard
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
